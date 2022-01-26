432b5f1ab108923280cafa74b53b82e309d17e12
RuoyuLiOT-DE <ruoyu.li@origi-tech.com>
2022-01-26 10:27:53 +0100

    pass build\

--- include/SamplePlugin.hpp
+++ include/SamplePlugin.hpp
@@ -12,7 +12,7 @@ class SamplePlugin: public rws::RobWorkStudioPlugin, private Ui::SamplePlugin
 Q_OBJECT
 Q_INTERFACES( rws::RobWorkStudioPlugin )
 #if RWS_USE_QT5
-Q_PLUGIN_METADATA(IID "dk.sdu.mip.Robwork.RobWorkStudioPlugin/0.1" FILE "plugin.json")
+Q_PLUGIN_METADATA(IID "dk.sdu.mip.Robwork.RobWorkStudioPlugin/0.1" FILE "../plugin.json")
 #endif
 public:
     SamplePlugin();
